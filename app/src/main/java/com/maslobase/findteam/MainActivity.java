629540bf89626af161c1deb5dc3f220a205a81ec
aptgetinstallme <inessa3110@gmail.com>
2017-09-07 22:11:13 +0200

    Added Steam OpenId authentication.

--- app/src/main/java/com/maslobase/findteam/MainActivity.java
+++ app/src/main/java/com/maslobase/findteam/MainActivity.java
@@ -1,14 +1,61 @@
 package com.maslobase.findteam;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String REALM_PARAM = "FindTeam";
+    String steamId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        final WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url,
+                                      Bitmap favicon) {
+                //checks the url being loaded
+                setTitle(url);
+                Uri Url = Uri.parse(url);
+
+                if (Url.getAuthority().equals(REALM_PARAM.toLowerCase())) {
+                    // That means that authentication is finished and the url contains user's id.
+                    webView.stopLoading();
+
+                    // Extracts user id.
+                    Uri userAccountUrl = Uri.parse(Url.getQueryParameter("openid.identity"));
+                    String userId = userAccountUrl.getLastPathSegment();
+
+                    // Do whatever you want with the user's steam id
+                    steamId = userId;
+                }
+            }
+
+        });
+        setContentView(webView);
+
+        // Constructing openid url request
+        String url = "https://steamcommunity.com/openid/login?" +
+                "openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&" +
+                "openid.identity=http://specs.openid.net/auth/2.0/identifier_select&" +
+                "openid.mode=checkid_setup&" +
+                "openid.ns=http://specs.openid.net/auth/2.0&" +
+                "openid.realm=https://" + REALM_PARAM + "&" +
+                "openid.return_to=https://" + REALM_PARAM + "/signin/";
+
+
+        webView.loadUrl(url);
     }
 }
